e4c7dec4d2e62e1fbf7f01a05a6f16b16ae30699
Amina Jusic <amina_jusic@yahoo.com>
2019-03-22 14:06:01 +0000

    Finnaly passed the test for noteController

--- spec/noteControllerSpec.js
+++ spec/noteControllerSpec.js
@@ -2,16 +2,34 @@ function NoteDouble() {
   this.text = 'test note'
 };
 
+function NoteDoubleTwo() {
+  this.text = 'test note laaaa la laaa laa laa laa la'
+};
+
 it("app div has no notes on load", function() {
   var noteController = new NoteController()
   noteController._setup()
-  assert.isEqual(document.getElementById('app').child, )
+  assert.isEqual(document.getElementById("app").innerHTML, "<ul></ul>")
 });
 
 it("app div has note text on load", function() {
-  var noteController = new NoteController(new NoteList())
-  var note = new NoteDouble
-  noteController._noteList.addNote(note)
+  var noteList = new NoteList()
+  var noteController = new NoteController(noteList)
+  var note = new NoteDouble()
+  noteList.addNote(note)
   noteController._setup()
-  assert.isEqual(document.getElementById('app').innerHTML, "<ul><li><div>test note</div></li></ul>")
+  var innerHtml = "<ul><li><div>test note</div></li></ul>"
+  assert.isEqual(document.getElementById("app").innerHTML, innerHtml)
+});
+
+it("app div has note text on load", function() {
+  var noteList = new NoteList()
+  var noteController = new NoteController(noteList)
+  var note = new NoteDouble()
+  var note2 = new NoteDoubleTwo()
+  noteList.addNote(note)
+  noteList.addNote(note2)
+  noteController._setup()
+  var innerHtml = "<ul><li><div>test note</div></li><li><div>test note laaaa la l</div></li></ul>"
+  assert.isEqual(document.getElementById("app").innerHTML, innerHtml)
 });
--- src/note-controller.js
+++ src/note-controller.js
@@ -2,21 +2,14 @@
   function NoteController(noteList = new NoteList()){
     this._noteList = noteList
     this._setup
-    // note = new Note('favourite drink')
-    // noteList.addNote(note)
-    // console.log(this._noteList)
+
   }
 
   NoteController.prototype = {
     _setup: function() {
-      var thisNoteController = this
-      window.addEventListener('load', function() {
-        // var note = new Note("This is great")
-        var noteListView = new NoteListView(thisNoteController._noteList)
-        // noteListView.list.addNote(note)
-        document.getElementById("app").innerHTML = noteListView.format()
-      });
+      var noteListView = new NoteListView(this._noteList)
+      document.getElementById("app").innerHTML = noteListView.format()
     }
   }
-exports.NoteController = NoteController
+  exports.NoteController = NoteController
 })(this);
